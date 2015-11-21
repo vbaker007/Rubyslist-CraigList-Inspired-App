c2e62f02ab7176aa3529bd18e14d746b11e52f65
vbaker007 <vbaker007@hotmail.com>
2015-11-21 00:00:11 -0500

    Added Terms.html.erb, Tips.html.erb, Privacy.html.erb, About.html.erb, Contact.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,22 @@ class PagesController < ApplicationController
 
   def help
   end
+
+  def scams
+  end
+
+  def tips
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def about
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   resources :categories
   resources :subcategories
   resources :listings
+
   root 'categories#index'
+
   match '/help', to: 'pages#help', via: :get
+  match '/scams', to: 'pages#scams', via: :get
+  match '/tips', to: 'pages#tips', via: :get
+  match '/terms', to: 'pages#terms', via: :get
+  match 'privacy', to: 'pages#privacy', via: :get
+  match '/about', to: 'pages#about', via: :get
+  match '/contact', to: 'pages#contact', via: :get
 end
