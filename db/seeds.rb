3c57be0c0c21f69f091f92f1da3f9baedba09738
vbaker007 <vbaker007@hotmail.com>
2015-11-23 00:28:45 -0500

    Added Community_Category to seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+community_category = Category.where(name: 'community').first_or_create(name: 'community')
