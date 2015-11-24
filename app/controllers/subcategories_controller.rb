8ef3ccb54a7614e47646d7a698d6bcd4d7a296fc
vbaker007 <vbaker007@hotmail.com>
2015-11-24 01:02:50 -0500

    Added Category_Subcategory Path to Links

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,3 +1,11 @@
 class SubcategoriesController < ApplicationController
-  
+ 
+  #def show
+  # @subcategory = Category.find(params[:id])
+  #end
+
+
+  #def show
+  #  @category = Subcategory.find(params[:id])
+  #end
 end
