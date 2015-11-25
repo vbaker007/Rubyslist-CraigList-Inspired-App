1f53cddf2271221370c23298e1468ee567e10c2f
vbaker007 <vbaker007@hotmail.com>
2015-11-24 21:59:53 -0500

    Added Def Find_by_category Method in Subcategories Controller

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -8,4 +8,10 @@ class SubcategoriesController < ApplicationController
   def show
    # @category = Subcategory.find(params[:id])
   end
+
+  def find_by_category
+    category = Category.find(params[:category_id])
+    subcategories = category.subcategories.find_all
+    render json: { subcategory: subcategories }
+  end
 end
