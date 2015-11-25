63b4ae15e2d35d4cc4c655dcbf2165a71879ad6d
vbaker007 <vbaker007@hotmail.com>
2015-11-25 17:18:57 -0500

    Added lat & lon columns to listing Model for geocoder gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,11 @@
 class Listing < ActiveRecord::Base
   belongs_to :category
   belongs_to :subcategory
+
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    [city, state, zipcode].join(', ')
+  end
 end
