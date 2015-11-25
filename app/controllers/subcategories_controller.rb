f29b8ec06c6194ff5dcbace8299e0ff2ccf2dd08
vbaker007 <vbaker007@hotmail.com>
2015-11-24 22:37:38 -0500

    added code to match corresponding subcategories to correct Category

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -12,6 +12,6 @@ class SubcategoriesController < ApplicationController
   def find_by_category
     category = Category.find(params[:category_id])
     subcategories = category.subcategories.find_all
-    render json: { subcategory: subcategories }
+    render json: { subcategories: subcategories }
   end
 end
